1a7acd57571e9b6b0cd4298cad297f44587ed5b3
Luca Liu <lucaliu96@gmail.com>
2022-10-15 18:01:21 +0200

    COMPLETED

--- test/spec.js
+++ test/spec.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const e = require("express");
 const app = require('supertest')(require('../app'));
 const {syncAndSeed, models} = require('../db');
 
@@ -26,10 +27,20 @@ describe("App", ()=> {
 
             const result = JSON.parse(response.text)
 
-            console.log(response.text)
             expect(response.status).to.equal(200);
             expect(movies.length).to.equal(result.length);
         });
-        //it('GET "/api/actors" working', async()=>)
+
+
+        it('GET "/api/actors" working', async()=>{
+            const actors = await models.Actor.findAll();
+            const response = await app.get('/api/actors');
+
+            const result = JSON.parse(response.text);
+
+            expect(response.status).to.equal(200);
+            expect(actors.length).to.equal(result.length);
+            expect(response.text).to.contain('Tom Hanks');
+        });
     })
 });
