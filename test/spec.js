92f48e216db2d9e17ff3481e7b2db06ca88cdeb5
Luca Liu <lucaliu96@gmail.com>
2022-10-15 17:10:07 +0200

    first travis

--- test/spec.js
+++ test/spec.js
@@ -3,7 +3,7 @@ const { expect } = require("chai")
 describe("App", ()=> {
     describe('Home Page', ()=>{
         it('1+1', () => {
-            const result = 1+0;
+            const result = 1+1;
             expect(2).to.equal(result);
         });
     });
