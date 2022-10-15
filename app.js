046f8f145e3c7b5b16b78f7ca204e12a8bf8a901
Luca Liu <lucaliu96@gmail.com>
2022-10-15 17:56:02 +0200

    api/movies test

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const {models} = require('./db');
 
 app.get('/', (req,res,next)=> {
     res.send(`HELLO`);
+});
+
+app.get('/api/movies', (req,res,next)=>{
+    models.Movie.findAll()
+        .then(movie => res.send(movie))
+        .catch(next)
+});
+
+app.get('/api/actors', (req,res,next)=>{
+    models.Actor.findAll()
+        .then(actor => res.send(actor))
+        .catch(next)
 });
 
 module.exports = app;
--- test/spec.js
+++ test/spec.js
@@ -12,11 +12,24 @@ describe("App", ()=> {
             expect(2).to.equal(result);
         });
     });
-    describe('GET "/" Route', ()=> {
+    describe('GET Routes', ()=> {
         it('GET "/" Route working', async()=>{
             const response = await app.get('/');
             expect(response.status).to.equal(200);
             expect(response.text).to.contain('HELLO');
-        })
+        });
+        it('GET "/api/movies" working', async()=>{
+            const movies = await models.Movie.findAll();
+            const response = await app.get('/api/movies');
+
+            //console.log(typeof JSON.parse(response.text));
+
+            const result = JSON.parse(response.text)
+
+            console.log(response.text)
+            expect(response.status).to.equal(200);
+            expect(movies.length).to.equal(result.length);
+        });
+        //it('GET "/api/actors" working', async()=>)
     })
 });
